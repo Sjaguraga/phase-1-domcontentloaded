cb745e3bc1d0f57bbc403f26829621857c602dd5
serref jaguraga <reef163@gmail.com>
2022-02-06 19:23:40 -0500

    commit tree d25ab875ec29e7c895b62da5175d67b5237c1d99

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded',function(){
+
+const p = document.querySelector('#text');
+p.textContent ='This is really cool!'
+
+    console.log('the DOM haas loaded')
+} )
+
